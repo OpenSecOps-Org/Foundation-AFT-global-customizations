70e179313401e1df6c578dea3fc3781226afb912
Peter Bengtson <peter@peterbengtson.com>
2024-09-18 13:33:05 +0200

    Commented-out bash code for Delegat's two open-source AFT addons

--- api_helpers/pre-api-helpers.sh
+++ api_helpers/pre-api-helpers.sh
@@ -1,3 +1,46 @@
-#!/bin/bash
+#!/bin/bash -e
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
 
 echo "Executing Pre-API Helpers"
+
+# echo
+
+# ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+# echo "Obtaining SSO Groups for account $ACCOUNT_ID..."
+# SSO_GROUPS=$(aws ssm get-parameters --names /aft/account-request/custom-fields/sso_groups --query "Parameters[0].Value")
+# echo "SSO Groups: $SSO_GROUPS"
+
+# echo "Obtaining SSO Users for account $ACCOUNT_ID..."
+# SSO_USERS=$(aws ssm get-parameters --names /aft/account-request/custom-fields/sso_users --query "Parameters[0].Value")
+# echo "SSO Users: $SSO_USERS"
+
+# echo "Posting SNS message to configure the account $ACCOUNT_ID for SSO access..."
+# aws sns publish --topic-arn "arn:aws:sns:eu-north-1:111111111111:aft-sso-account-configuration-topic" \
+#   --message "{\"account_id\": \"$ACCOUNT_ID\", \"sso_groups\": $SSO_GROUPS, \"sso_users\": $SSO_USERS}"
+
+# echo
+
+# echo "Obtaining subdomain delegations for account $ACCOUNT_ID..."
+# SUBDOMAIN_DELEGATIONS=$(aws ssm get-parameters --names /aft/account-request/custom-fields/subdomain_delegations --query "Parameters[0].Value")
+# echo "Subdomain delegations: $SUBDOMAIN_DELEGATIONS"
+
+# echo "Obtaining subdomain delegations to remove for account $ACCOUNT_ID..."
+# SUBDOMAIN_DELEGATIONS_TO_REMOVE=$(aws ssm get-parameters --names /aft/account-request/custom-fields/subdomain_delegations_to_remove --query "Parameters[0].Value")
+# echo "Subdomain delegations to remove: $SUBDOMAIN_DELEGATIONS_TO_REMOVE"
+
+# echo "Posting SNS message to configure subdomain delegations for the account $ACCOUNT_ID..."
+# aws sns publish --topic-arn "arn:aws:sns:eu-north-1:111111111111:aft-subdomain-delegation-topic" \
+#   --message "{\"account_id\": \"$ACCOUNT_ID\", \"subdomain_delegations\": $SUBDOMAIN_DELEGATIONS, \"subdomain_delegations_to_remove\": $SUBDOMAIN_DELEGATIONS_TO_REMOVE}"
+
+# echo
+
+# echo "Posting SNS message to configure Jira board for the account $ACCOUNT_ID..."
+# aws sns publish --topic-arn "arn:aws:sns:eu-north-1:111111111111:aft-jira-board-configuration-topic" \
+#   --message "{\"account_id\": \"$ACCOUNT_ID\"}"
+
+# echo
+
+
